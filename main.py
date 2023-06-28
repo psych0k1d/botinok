2a2d171135d0415fc5d65126af25625fc68ca793
nnXXNXNXS <131406295+psych0k1d@users.noreply.github.com>
2023-06-28 17:36:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import telebot
 from random import randint
 import json
-
+from telebot import types
 with open('data.json', 'r', encoding='utf-8') as file:
     data = json.load(file)
     token = data["token"]
@@ -15,10 +15,19 @@ def user_checker(id_):
         open("id.txt", 'w').write(f"{str(id_)} ")
 
 
+def simen():
+    print("lol")
+
 @bot.message_handler(commands=['start'])
 def commands(m):
-    bot.send_message(m.from_user.id, 'helo')
+    mrkup = types.InlineKeyboardMarkup()
+    mrkup.add(types.InlineKeyboardButton("потеребить ананас", callback_data=f"simen"))
+    bot.send_message(m.from_user.id, 'helo', reply_markup=mrkup)
     user_checker(m.from_user.id)
 
+@bot.callback_query_handler(func=lambda callback: True)
+def cumback(callback):
+    if callback.data == "simen":
+        commands(callback)
 
 bot.infinity_polling()
