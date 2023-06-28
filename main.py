b23a41c1a0d4d3e53a806c1d836398e5877e2f6f
nnXXNXNXS <131406295+psych0k1d@users.noreply.github.com>
2023-06-28 14:43:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import telebot
+import json
 
-def main():
-    pass
+with open('data.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+    token = data["token"]
+
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(commands=['start'])
+def commands(m):
+    bot.send_message(m.from_user.id, 'helo')
 
 
 
-if __name__ == "__main__":
-    main()
+bot.infinity_polling()
